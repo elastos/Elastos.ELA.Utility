e543609101f0ddee778088417930f9782ce95252
jiangzehua <jiangzehua@elastos.org>
2018-11-12 18:54:41 +0800

    add confirm message for dpos

---
+++ p2p/msg/confirm.go
@@ -0,0 +1,31 @@
+package msg
+
+import (
+	"io"
+
+	"github.com/elastos/Elastos.ELA.Utility/p2p"
+	"github.com/elastos/Elastos.ELA/dpos/chain"
+)
+
+const MaxConfirmSize = 8276 //32+36*(33+32+65+33+65)
+
+type Confirm struct {
+	Command  string
+	Proposal chain.ProposalVoteSlot
+}
+
+func (msg *Confirm) CMD() string {
+	return p2p.CmdConfirm
+}
+
+func (msg *Confirm) MaxLength() uint32 {
+	return MaxConfirmSize
+}
+
+func (msg *Confirm) Serialize(w io.Writer) error {
+	return msg.Proposal.Serialize(w)
+}
+
+func (msg *Confirm) Deserialize(r io.Reader) error {
+	return msg.Proposal.Deserialize(r)
+}
